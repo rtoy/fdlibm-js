b53c853559e2262e695a16a03c349630add743ec
Raymond Toy <rtoy@chromium.org>
2015-02-25 08:51:06 -0800

    Move scalbn from payne-hanek.js to scalbn.js.

--- payne-hanek.js
+++ payne-hanek.js
@@ -154,41 +154,6 @@ function copysign(x, y)
 
 // Compute x*2^n using exponent manipulation instead of exponentiation
 // or multiplication.
-var huge = 1e300;
-var tiny = 1e-300;
-var two54 = Math.pow(2, 54);
-
-function scalbn(x, n)
-{
-    var  k,hx,lx;
-    hx = _DoubleHi(x);
-    lx = _DoubleLo(x);
-    k = (hx&0x7ff00000)>>20;		/* extract exponent */
-    if (k==0) {				/* 0 or subnormal x */
-        if ((lx|(hx&0x7fffffff))==0) return x; /* +-0 */
-        x *= two54; 
-        hx = _DoubleHi(x);
-        k = ((hx&0x7ff00000)>>20) - 54; 
-        if (n< -50000) return tiny*x; 	/*underflow*/
-    }
-    if (k==0x7ff) return x+x;		/* NaN or Inf */
-    k = k+n; 
-    if (k >  0x7fe) return huge*copysign(huge,x); /* overflow  */
-    if (k > 0) { 				/* normal result */
-        //_DoubleHi(x) = (hx&0x800fffff)|(k<<20);
-        x = _ConstructDouble((hx&0x800fffff)|(k<<20), _DoubleLo(x));
-        return x;
-    }
-    if (k <= -54)
-        if (n > 50000) 	/* in case integer overflow in n+k */
-            return huge*copysign(huge,x);	/*overflow*/
-        else return tiny*copysign(tiny,x); 	/*underflow*/
-    k += 54;				/* subnormal result */
-    //__HI(x) = (hx&0x800fffff)|(k<<20);
-    x = _ConstructDouble((hx&0x800fffff)|(k<<20), _DoubleLo(x));
-    return x*twom54;
-}
-
 function kernel_rem_pio2(x, y, e0, nx, prec, ipio2)
 {
     var jz,jx,jv,jp,jk,carry,n;
